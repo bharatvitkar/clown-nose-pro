94b2f9de2cdb3d66b8b7511bd08e31f6ce99c699
bharatvitkar <79301656+bharatvitkar@users.noreply.github.com>
2022-08-28 09:38:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,10 +7,33 @@ function setup()
 {
     canvas=createCanvas(290,290);
     canvas.center();
+
+    video=createCapture(VIDEO);
+    video.size(290,290);
+    video.hide();
+
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        console.log("nose x = "+results[0].pose.nose.x);
+        console.log("nose y = "+results[0].pose.nose.y);
+    }
+}
+
+function modelLoaded()
+{
+    console.log('posenet model is initialized');
 }
 
 function draw()
 {
+image(video,0,0,290,290);
 
 }
 
